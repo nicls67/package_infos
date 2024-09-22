3e39082233c32e4e7210aa5d558d339c5ebd2901
Nicolas SIMON <nicolas.simon6790@gmail.com>
2024-09-22 18:23:33 +0200

    Add Rust IDE configurations and new package info macro
    Introduce a new macro `pkg_infos` for generating `PackageInfos` structures, which include metadata and dependencies of Rust packages. Also, add IDE configurations for Rust formatting settings to enhance the development environment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,52 @@
-
-
+/// A structure to hold information about a package.
+///
+/// # Fields
+/// - `name`: The name of the package.
+/// - `version`: The version of the package.
+/// - `authors`: The authors of the package.
+/// - `description`: The description of the package.
+/// - `dependencies`: A list of dependencies for the package.
 pub struct PackageInfos {
-    name: String,
-    version: String,
-    author: String,
-    description: String,
-    dependencies: Vec<PackageInfos>
+    pub name: &'static str,
+    pub version: &'static str,
+    pub authors: &'static str,
+    pub description: &'static str,
+    pub dependencies: Vec<PackageInfos>,
+}
+
+#[macro_export]
+macro_rules! pkg_infos {
+    ( $( $x:expr ),* ) => {
+            /// Constructs a `PackageInfos` structure with metadata and dependencies.
+            ///
+            /// This macro gathers information about the package from the environment variables
+            /// set by Cargo, and also includes the provided dependencies.
+            ///
+            /// # Arguments
+            ///
+            /// - `$( $x:expr ),*`: A comma-separated list of expressions that each return a `PackageInfos` structure,
+            /// representing the dependencies of the package.
+            ///
+            /// # Returns
+            ///
+            /// A `PackageInfos` structure containing metadata about the package and its dependencies.
+            pub fn get_package_infos() -> PackageInfos {
+                let name = env!("CARGO_PKG_NAME");
+                let version = env!("CARGO_PKG_VERSION");
+                let authors = env!("CARGO_PKG_AUTHORS");
+                let description = env!("CARGO_PKG_DESCRIPTION");
+                let mut dependencies = Vec::new();
+                $(
+                    dependencies.push($x.get_package_infos());
+                )*
+
+                PackageInfos {
+                    name,
+                    version,
+                    authors,
+                    description,
+                    dependencies
+                }
+            }
+    };
 }
