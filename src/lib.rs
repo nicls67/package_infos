41293b85fc175c799d830030ad1cfe526efc7436
Nicolas SIMON <nicolas.simon6790@gmail.com>
2024-09-25 16:03:13 +0200

    Implement fmt::Display for PackageInfos
    Added the fmt::Display trait implementation to the PackageInfos structure for better readability and formatted output. Also, updated the pkg_infos macro to improve dependency handling and consistency.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// A structure to hold information about a package.
 ///
 /// # Fields
@@ -15,31 +17,55 @@ pub struct PackageInfos {
     pub dependencies: Vec<PackageInfos>,
 }
 
+impl fmt::Display for PackageInfos {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut text = format!(
+            "{} crate version {}\n   {}\n   Authors : {}",
+            self.name, self.version, self.description, self.authors
+        );
+        if !self.dependencies.is_empty() {
+            text = format!("{}\n\n   Using librairies :", text);
+        }
+        for dependency in self.dependencies.iter() {
+            let binding = dependency.to_string();
+            let mut split = binding.split('\n').collect::<Vec<&str>>();
+            let first = split.remove(0);
+            text = format!("{}\n      * {}", text, first);
+            for line in split.iter() {
+                text = format!("{}\n        {}", text, line);
+            }
+        }
+        writeln!(f, "{}", text)
+    }
+}
+
+/// Constructs a `PackageInfos` structure with metadata and dependencies.
+///
+/// This macro gathers information about the package from the environment variables
+/// set by Cargo, and also includes the provided dependencies.
+///
+/// # Arguments
+///
+///  A comma-separated list of expressions that each return a `PackageInfos` structure,
+///  representing the dependencies of the package.
+///
+/// # Returns
+///
+/// A `PackageInfos` structure containing metadata about the package and its dependencies.
 #[macro_export]
 macro_rules! pkg_infos {
-    ( $( $x:expr ),* ) => {
-            /// Constructs a `PackageInfos` structure with metadata and dependencies.
-            ///
-            /// This macro gathers information about the package from the environment variables
-            /// set by Cargo, and also includes the provided dependencies.
-            ///
-            /// # Arguments
-            ///
-            ///  * `$( $x:expr ),*`: A comma-separated list of expressions that each return a `PackageInfos` structure,
-            ///    representing the dependencies of the package.
-            ///
-            /// # Returns
-            ///
-            /// A `PackageInfos` structure containing metadata about the package and its dependencies.
+    ( $( $x:ident ),* ) => {
+
             pub fn get_package_infos() -> PackageInfos {
                 let name = env!("CARGO_PKG_NAME");
                 let version = env!("CARGO_PKG_VERSION");
                 let authors = env!("CARGO_PKG_AUTHORS");
                 let description = env!("CARGO_PKG_DESCRIPTION");
-                let mut dependencies = Vec::new();
+                let dependencies = vec![
                 $(
-                    dependencies.push($x.get_package_infos());
+                    $x::get_package_infos(),
                 )*
+                ];
 
                 PackageInfos {
                     name,
